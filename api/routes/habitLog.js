4925f50280185906fbb756e9b78d74e0377e5643
Christina Kaden <christina.kaden@gmx.de>
2026-02-13 18:01:50 +0100

    get tracked habits per day and save in state, prepared styling of boxes for habits in calendar (wip)

--- api/routes/habitLog.js
+++ api/routes/habitLog.js
@@ -1,9 +1,13 @@
 const router = require("express").Router();
 const HabitLog = require("../models/HabitLog.js");
 
-router.get("/", async (req, res) => {
+router.get("/gettracked/:first_day/:last_day", async (req, res) => {
+  const { first_day, last_day } = req.params
+  console.log(first_day, last_day, typeof first_day, typeof last_day);
   try {
-    const hab = await HabitLog.find();
+    const hab = await HabitLog.find({
+      date: { $gte: first_day, $lte: last_day }, //$gte (>=), $lte (<=)
+    });
     res.send({ ok: true, data: hab });
   } catch (error) {
     res.send({ ok: false, message: error });
@@ -11,20 +15,18 @@ router.get("/", async (req, res) => {
 });
 
 router.post("/newlog", async (req, res) => {
-  const { date,
-          habit_id,
-          habit_name } = req.body
+  const { date, habit_id, habit_name } = req.body;
   try {
     const newlog = {
       date: date,
       habit_id: habit_id,
       habit_name: habit_name,
-    }
-    const hablog = await HabitLog.create(newlog)
-    res.send({ok: true, data: hablog})
-  } catch(error) {
-    res.send({ok: false, message: error})
+    };
+    const hablog = await HabitLog.create(newlog);
+    res.send({ ok: true, data: hablog });
+  } catch (error) {
+    res.send({ ok: false, message: error });
   }
-})
+});
 
 module.exports = router;
--- src/components/Month.jsx
+++ src/components/Month.jsx
@@ -2,11 +2,7 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import URL from "../config.js";
 import Week from "../components/Week";
-import {
-  Dialog,
-  DialogContent,
-  DialogTitle,
-} from "@mui/material";
+import { Dialog, DialogContent, DialogTitle } from "@mui/material";
 
 function Month({ habits }) {
   const [selectedMonthIndex, setSelectedMonthIndex] = useState(
@@ -19,16 +15,25 @@ function Month({ habits }) {
   const [daysOfMonth, setDaysOfMonth] = useState([]);
   const [open, setOpen] = useState(false);
   const [checked, setChecked] = useState({}); // {1: true, 2: false, 3: true}
+  const [trackedOfMonth, setTrackedOfMonth] = useState([])
+  const [habitsPerDay, setHabitsPerDay] = useState({});
+
 
   //console.log(selectedMonthIndex);
   //console.log(selectedDay);
   //console.log(checked);
   //console.log(habits)
+  console.log(trackedOfMonth)
+  console.log(habitsPerDay)
 
   const now = new Date();
   const year = now.getFullYear();
   const month = now.getMonth(); //index for month
   const days = new Date(year, month + 1, 0).getDate();
+  const first_day = new Date(year, month, 1);
+  const last_day = new Date(year, month, days);
+  //console.log(first_day)
+  //console.log(last_day)
 
   const trackHabits = async () => {
     const checkedHabits = Object.keys(checked)
@@ -42,7 +47,7 @@ function Month({ habits }) {
     try {
       for (let i = 0; i < arr_habits.length; i++) {
         //console.log(selectedDay, arr_habits[i]._id, arr_habits[i].name)
-        const habitlog = await axios.post(`${URL}/habitlog/newlog`, {
+        await axios.post(`${URL}/habitlog/newlog`, {
           date: selectedDay,
           habit_id: arr_habits[i]._id,
           habit_name: arr_habits[i].name,
@@ -53,10 +58,30 @@ function Month({ habits }) {
     }
   };
 
+  const getTrackedHabits = async () => {
+    try {
+      const response = await axios.get(`${URL}/habitlog/gettracked/${first_day}/${last_day}`);
+      //console.log(response.data.data)
+      setTrackedOfMonth(response.data.data)
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    const map = {}
+    for (const log of trackedOfMonth) {
+      const day = new Date(log.date).toISOString().slice(0, 10); // "2026-02-13"
+      if (!map[day]) {
+        map[day] = []
+      }
+      map[day].push(log.habit_id)
+    }
+    setHabitsPerDay(map)
+  }, [trackedOfMonth])
+
   useEffect(() => {
     //finding out the weekday of first of selected month - sunday 0 to saturday 6
-    const first_day = new Date(year, month, 1);
-    //console.log(first_day)
     let first_weekday = first_day.getDay();
     if (first_weekday === 0) {
       first_weekday = 7;
@@ -72,6 +97,7 @@ function Month({ habits }) {
       arr_days.push(i);
     }
     setDaysOfMonth(arr_days);
+    getTrackedHabits();
   }, []);
 
   return (
@@ -92,9 +118,28 @@ function Month({ habits }) {
                 : undefined
             }
           >
-            <p className={d === new Date().getDate() ? "calendar-today" : ""}>
-              {d}
-            </p>
+            <div>
+              <p className="calendar-today">
+                <span
+                  className={
+                    d === new Date().getDate() ? "calendar-today-inner" : ""
+                  }
+                >
+                  {d}
+                </span>
+              </p>
+            </div>
+            <div>
+              <div
+                style={{
+                  width: 12,
+                  height: 12,
+                  backgroundColor: "black",
+                  borderRadius: 3,
+                  marginRight: 8,
+                }}
+              ></div>
+            </div>
           </article>
         ))}
       </section>
